dfa867ed0e3a8324d22befc17ec5ba2428e46810
rubyury <ruben.garloz00@gmail.com>
2024-11-08 12:37:02 -0600

    idk

--- examen_practica/sql/2.DATOS.sql
+++ examen_practica/sql/2.DATOS.sql
@@ -2,11 +2,11 @@
 use TABLA;
 
 
-insert into ARTICULO (Clave,articulo,descripcion,precio) values ( 1, 'minor', 'mmmmm', '150' );
-insert into ARTICULO (Clave,articulo,descripcion,precio) values ( 2, 'child', 'para perro', '250' );
-insert into ARTICULO (Clave,articulo,descripcion,precio) values ( 3, 'woman de pez', 'si', '50' );
-insert into ARTICULO (Clave,articulo,descripcion,precio) values ( 4, 'sme de pescado', 'para gato', '100' );
-insert into ARTICULO (Clave,articulo,descripcion,precio) values ( 5, 'cepillin', 'para todo tipo de pelo de animal', '150' );
+insert into ARTICULO (Clave,articulo,descripcion,precio) values ( 1, 'objeto', 'descripcion', '150' );
+insert into ARTICULO (Clave,articulo,descripcion,precio) values ( 2, 'objeto', 'descripcion', '150' );
+insert into ARTICULO (Clave,articulo,descripcion,precio) values ( 3, 'objeto', 'descripcion', '150' );
+insert into ARTICULO (Clave,articulo,descripcion,precio) values ( 4, 'objeto', 'descripcion', '150' );
+insert into ARTICULO (Clave,articulo,descripcion,precio) values ( 5, 'objeto', 'descripcion', '150' );
 
 
 
